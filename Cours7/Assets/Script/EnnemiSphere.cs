0a23fa3f668a95a338f7d2bf0b9b8b21d6e84230
Johnatan <djohnat_13@hotmail.com>
2019-09-17 10:38:25 -0400

    Spawner des ennemies

--- Cours7/Assets/Script/EnnemiSphere.cs
+++ Cours7/Assets/Script/EnnemiSphere.cs
@@ -2,37 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnnemiCube : Ennemi
-{
+public class EnnemiSphere : Ennemi {
     GameObject player;
     public float speed = 5;
     Rigidbody rbd;
     AudioSource audioSource;
     public AudioMusic audioMusic;
-    public GameObject particuleSysteme;
-    public override void Die()
-    {
-        audioSource.PlayOneShot(audioMusic.soundToPlay);
-        Instantiate(particuleSysteme, gameObject.transform.position, Quaternion.identity);
-        Destroy(gameObject);
-    }
-
-    // Use this for initialization
-    void Start () {
+    public GameObject EnnemiSpherePetit;
+    public int NbrMaxEnnemiSphereApresMort = 2;
+	// Use this for initialization
+	void Start () {
         player = GameObject.FindGameObjectWithTag("Player");
         rbd = GetComponent<Rigidbody>();
         audioSource = GameObject.FindGameObjectWithTag("SounPlayer").GetComponent<AudioSource>();
-
-    }
-
-    // Update is called once per frame
-    void Update () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
         FollowPlayer();
 
-
-    }
+	}
     private void FollowPlayer()
     {
         rbd.MovePosition(Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime));
+    }
+
+    public override void Die()
+    {
+        audioSource.PlayOneShot(audioMusic.soundToPlay);
+        for (int i = 0; i < NbrMaxEnnemiSphereApresMort; i++)
+        {
+            Instantiate(gameObject, (gameObject.transform.position + Vector3.forward *i* 5), Quaternion.identity);
+        }
+        Destroy(gameObject);
     }
 }
---
+++ Cours7/Assets/Script/Spawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+    private float timeLeftBeforeSpawn;
+    public float spawnRate = 1;
+    private SpawnPoint[] spawnPoints;
+    public GameObject[] ennemiPrefab;
+    // Use this for initialization
+    void Start() {
+        spawnPoints = FindObjectsOfType<SpawnPoint>();
+        timeLeftBeforeSpawn = 1 / spawnRate;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateSysteme();
+
+    }
+    public void UpdateSysteme()
+    {
+        timeLeftBeforeSpawn -= Time.deltaTime;
+        if (timeLeftBeforeSpawn <0)
+        {
+            SpawnEnnemi();
+            timeLeftBeforeSpawn = 1 / spawnRate;
+        }
+    }
+    public void SpawnEnnemi()
+    {
+        int countSpawnPoint = spawnPoints.Length;
+        int randomPointIndex = Random.Range(0, countSpawnPoint);
+        SpawnPoint spawnPointRandomSelected = spawnPoints[randomPointIndex];
+        GameObject newCube = Instantiate(ennemiPrefab[Random.Range(0,2)], spawnPointRandomSelected.GetPosition(), spawnPointRandomSelected.transform.rotation);
+    }
+}
